b93d6e9aa177fe51a1532c5f2eef77e8aafb2c58
Maximillian Agwaigbo <agwaigbomaximillian@gmail.com>
2022-11-24 11:24:22 +0100

    return a throwable instead of string

--- app/src/main/java/com/asterisk/infoflow/commons/Resource.kt
+++ app/src/main/java/com/asterisk/infoflow/commons/Resource.kt
@@ -5,6 +5,6 @@ sealed class Resource<T>(
     val error: String? = null
 ) {
     class Success<T>(data: T) : Resource<T>(data)
-    class Error<T>(error: String, data: T? = null) : Resource<T>(data, error)
+    class Error<T>(error: String, data: T) : Resource<T>(data, error)
     class Loading<T>(data: T?) : Resource<T>(data)
 }
